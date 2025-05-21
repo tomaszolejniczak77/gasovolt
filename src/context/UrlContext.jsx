f1c29fb4e3c8915054e0dc2f9c389f2918437a57
tomaszolejniczak77 <o.tomek@wp.pl>
2025-05-21 10:50:36 +0200

    URL back to http

--- src/context/UrlContext.jsx
+++ src/context/UrlContext.jsx
@@ -5,7 +5,7 @@ const UrlContext = createContext();
 export const UrlProvider = ({ children }) => {
   const urls = {
     // baseUrl: "https://gasovoltserver-production.up.railway.app",
-    baseUrl: "https://192.168.1.225:8000",
+    baseUrl: "http://192.168.1.225:8000",
     usageElectricityEndpoint: "usage/electricity",
     usageGasEndpoint: "usage/gas",
     deleteLastGasEndpoint: "delete_last/gas_usage",
