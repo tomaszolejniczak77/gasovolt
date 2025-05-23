c773aab13492048301c03e1639627a15d88288ab
tomaszolejniczak77 <o.tomek@wp.pl>
2025-05-23 09:21:13 +0200

    cloudflare tunel updated

--- src/context/UrlContext.jsx
+++ src/context/UrlContext.jsx
@@ -6,7 +6,7 @@ export const UrlProvider = ({ children }) => {
   const urls = {
     // baseUrl: "https://gasovoltserver-production.up.railway.app",
     // baseUrl: "http://192.168.1.225:8000",
-    baseUrl: "https://high-powell-nevertheless-mc.trycloudflare.com",
+    baseUrl: "https://active-garage-e-stake.trycloudflare.com",
     usageElectricityEndpoint: "usage/electricity",
     usageGasEndpoint: "usage/gas",
     deleteLastGasEndpoint: "delete_last/gas_usage",
