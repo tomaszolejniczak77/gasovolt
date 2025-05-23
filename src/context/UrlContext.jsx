bd5e6743771bab8ff09059c69facf1c8ed4b114f
tomaszolejniczak77 <o.tomek@wp.pl>
2025-05-23 10:45:27 +0200

    update tunel

--- src/context/UrlContext.jsx
+++ src/context/UrlContext.jsx
@@ -6,7 +6,7 @@ export const UrlProvider = ({ children }) => {
   const urls = {
     // baseUrl: "https://gasovoltserver-production.up.railway.app",
     // baseUrl: "http://192.168.1.225:8000",
-    baseUrl: "https://active-garage-e-stake.trycloudflare.com",
+    baseUrl: "https://click-subjects-stainless-lower.trycloudflare.com",
     usageElectricityEndpoint: "usage/electricity",
     usageGasEndpoint: "usage/gas",
     deleteLastGasEndpoint: "delete_last/gas_usage",
