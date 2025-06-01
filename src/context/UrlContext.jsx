be4137f9f725243f09ec0e4f9ca4d2b5e40aacd2
tomaszolejniczak77 <o.tomek@wp.pl>
2025-06-01 10:36:59 +0200

    url change

--- src/context/UrlContext.jsx
+++ src/context/UrlContext.jsx
@@ -5,8 +5,7 @@ const UrlContext = createContext();
 export const UrlProvider = ({ children }) => {
   const urls = {
     // baseUrl: "https://gasovoltserver-production.up.railway.app",
-    // baseUrl: "http://192.168.1.225:8000",
-    baseUrl: "https://click-subjects-stainless-lower.trycloudflare.com",
+    baseUrl: "http://192.168.1.225:5000",
     usageElectricityEndpoint: "usage/electricity",
     usageGasEndpoint: "usage/gas",
     deleteLastGasEndpoint: "delete_last/gas_usage",
